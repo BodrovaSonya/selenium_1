3927d4c80bc1efd44005bc27fe7549ba372a9f88
BodrovaSonya <bodrova_sonya@mail.ru>
2018-01-16 17:16:42 +0300

    Add functions

--- selenium1/src/test/java/SberbankTest.java
+++ selenium1/src/test/java/SberbankTest.java
@@ -1,9 +1,31 @@
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
 
 public class SberbankTest {
+    WebDriver driver;
+    String baseUrl;
+
+    @Before
+    public void beforeTest(){
+        System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
+        baseUrl = "http://www.sberbank.ru/ru/person";
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+    }
 
     @Test
     public void testSberbank(){
-        System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
+
+    }
+
+    @After
+    public void afterTest(){
+        driver.quit();
     }
 }
