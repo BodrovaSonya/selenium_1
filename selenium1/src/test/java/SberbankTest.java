f05556c2f2b49c7bfb104a528cc59db00ed832df
BodrovaSonya <bodrova_sonya@mail.ru>
2018-01-16 09:38:44 +0300

    16/01/18

---
+++ selenium1/src/test/java/SberbankTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class SberbankTest {
+
+    @Test
+    public void testSberbank(){
+        System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
+    }
+}
