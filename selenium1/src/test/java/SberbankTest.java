9f814a0e7627178561c1753dddd901128257558d
BodrovaSonya <sophie.bodrova@gmail.com>
2018-01-19 00:55:04 +0300

    19/01/18

--- selenium1/src/test/java/SberbankTest.java
+++ selenium1/src/test/java/SberbankTest.java
@@ -1,7 +1,7 @@
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.Before;
 import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class SberbankTest {
@@ -21,19 +22,18 @@ public class SberbankTest {
         System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
         baseUrl = "http://www.sberbank.ru/ru/person";
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get(baseUrl);
     }
 
     @Test
     public void testSberbank(){
-        Wait<WebDriver> wait = new WebDriverWait(driver, 5, 1000);
+        Wait wait = new WebDriverWait(driver, 5, 1000);
         driver.findElement(By.xpath(".//div[@class=\"sbrf-div-list-inner --area bp-area header-container\"]//a[@aria-label=\"Раздел Застраховать себя  и имущество\"]")).click();
-        // //li[5]//*[@class="alt-menu-collapser__area alt-menu-collapser__area_cols_3"]
-        WebElement menu = driver.findElement(By.xpath("//*[@class='sbrf-rich-outer']/h1"));
-        wait.until(ExpectedConditions.visibilityOf(menu));
-        driver.findElement(By.xpath(".//div[@class=\"sbrf-div-list-inner --area bp-area header-container\"]//a[contains(text(),'Страхование путешественников')]")).click();
+        WebElement menuItem = driver.findElement(By.xpath("//div[@class=\"sbrf-div-list-inner --area bp-area header-container\"]//a[contains(text(),'Страхование путешественников')]"));
+        wait.until(ExpectedConditions.visibilityOf(menuItem));
+        driver.findElement(By.xpath("//div[@class=\"sbrf-div-list-inner --area bp-area header-container\"]//a[contains(text(),'Страхование путешественников')]")).click();
 
         driver.manage().timeouts().pageLoadTimeout(60,TimeUnit.SECONDS);
 
@@ -42,13 +42,18 @@ public class SberbankTest {
         wait.until(ExpectedConditions.visibilityOf(title));
         Assert.assertEquals("Страхование путешественников",title.getText());
 
-        driver.findElement(By.xpath("//a[@href=\"https://online.sberbankins.ru/store/vzr/index.html\"][not(@class)]")).click();
+        driver.findElement(By.xpath("//img[@src=\"/portalserver/content/atom/contentRepository/content/person/travel/banner-zashita-traveler.jpg?id=f6c836e1-5c5c-4367-b0d0-bbfb96be9c53\"]")).click();
         driver.manage().timeouts().pageLoadTimeout(60,TimeUnit.SECONDS);
-        String childHandle = "https://online.sberbankins.ru/store/vzr/index.html";
-        driver.switchTo().window(childHandle);
-        //driver.manage().timeouts().pageLoadTimeout(60,TimeUnit.SECONDS);
+        String parentWindow = driver.getWindowHandle();
+        Set<String> handles =  driver.getWindowHandles();
+        for(String windowHandle  : handles) {
+            if (!windowHandle.equals(parentWindow)) {
+                driver.switchTo().window(windowHandle);
+            }
+        }
+        driver.manage().timeouts().pageLoadTimeout(60,TimeUnit.SECONDS);
 
-        driver.findElement(By.xpath("//div[1]/div[@class=\"b-form-prog-box\"]")).click();
+        driver.findElement(By.xpath("//*[contains(text(),\"Минимальная\")]")).click();
         driver.findElement(By.xpath("//span[@class=\"b-button-block-center\"]//*[contains(text(),\"Оформить\")]")).click();
 
         //застрахованный
@@ -79,7 +84,7 @@ public class SberbankTest {
                 driver.findElement(By.name("name")).getAttribute("value"));
         Assert.assertEquals("Петрович",
                 driver.findElement(By.name("middlename")).getAttribute("value"));
-        Assert.assertEquals("01.01.1989",
+        Assert.assertEquals("",
                 driver.findElement(By.name("birthDate")).getAttribute("value"));
         Assert.assertEquals("1234",
                 driver.findElement(By.name("passport_series")).getAttribute("value"));
